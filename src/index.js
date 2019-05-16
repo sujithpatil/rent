db1ccd729d0c5127e581304b53b64c14e48b3ece
Uchiha Itachi <sujithpatil5@gmail.com>
2019-05-16 16:11:02 +0530

    Downloading file

--- src/index.js
+++ src/index.js
@@ -2,9 +2,30 @@ import React , {Component} from 'react';
 import ReactDom from "react-dom";
 import data from './app.json';
 import './app.scss';
+import html2canvas from 'html2canvas';
 
 class Rent extends Component {
     
+    downloadFile() {
+
+        const input = document.getElementById('rent');
+        
+        html2canvas(input).then(canvas=>{
+            
+            const imgData = canvas.toDataURL('image/png');
+            const base_image = new Image();
+            base_image.src = imgData;
+
+            var link = document.createElement('a');
+            link.href = imgData;
+            link.download = 'receipt.jpg';
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+
+        });
+    }
+
     render() {        
         const markUp = <div className='rent-container'>
                             <h2 className='title'>{data.rent}</h2>
@@ -36,9 +57,8 @@ class Rent extends Component {
                         </div>;
         return <React.Fragment>
                    {markUp}
-                   <button className='download'>{data.download}</button>
-                </React.Fragment>
-        ;
+                   <button className='download' onClick={this.downloadFile}>{data.download}</button>
+                </React.Fragment>;
     }
 }
 
